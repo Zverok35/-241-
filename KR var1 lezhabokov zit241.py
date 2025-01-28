4046ac4235b88d5577c1f83af382001a32ca0169
Zverok35 <zsum4151@gmail.com>
2025-01-29 00:02:10 +0300

    Update KR var1 lezhabokov zit241.py

--- KR var1 lezhabokov zit241.py
+++ KR var1 lezhabokov zit241.py
@@ -1,6 +1,14 @@
 def print_numbers(A, B):
-    for number in range(A, B + 1):
-        print(number)
-A = 3
-B = 7
-print_numbers(A, B)
+    for num in range(A, B + 1):
+        print(num, end=' ')
+
+# Ввод значений A и B
+A = int(input("Введите число A: "))
+B = int(input("Введите число B: "))
+
+# Проверка условия A ≤ B
+if A <= B:
+    print("Числа от A до B включительно:")
+    print_numbers(A, B)
+else:
+    print("Ошибка: A должно быть меньше или равно B").
